fc6de1d22f0561c98f1bd3cc0d928bdf0f4ca262
twocowsong <twocowsong@naver.com>
2024-10-20 16:54:21 +0900

    state 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // let post = '강남 고기 맛집';
+    let [titles, set]= useState(
+        [
+            {title : '남자 코트 추천', goodCnt : 0},
+            {title : '우동 맛집', goodCnt : 0},
+            {title : '파이썬 독학', goodCnt : 0}
+    ]);
+
+    function goodCntAdd(index) {
+        set(obj => {
+            const newObj = [...obj];
+            obj[index] = {...obj[index], goodCnt : newObj[index].goodCnt + 1};
+            return newObj;
+        })
+    }
+
+    function change(index) {
+        set(obj => {
+            const objElement = [...obj];
+
+            for (const target of objElement) {
+                if (target.title === '남자 코트 추천') {
+                    target.title = '여자 코트 추천';
+                }
+            }
+
+            return objElement;
+        })
+    }
+
+    return (
+        <div className="App">
+
+            <div className="black-nav">
+                <h4 style={{ fontSize: '16px'}}>React blog</h4>
+            </div>
+
+            {titles.map((obj, index) => (
+                <div className="list" key={index}>
+                    <button onClick={() => change(index)}>{obj.title} 변경</button>
+                    <h4>{obj.title} <span onClick={() => goodCntAdd(index)}>👍🏻</span> {obj.goodCnt} </h4>
+                    <p>2월 17일 발행</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
